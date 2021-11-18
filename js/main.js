2ea2478cd8417a86e0cecda384bd1a15e537a3c3
djeday <gvozdetskyidev@gmail.com>
2021-11-18 16:23:21 +0200

    added language bar

--- js/main.js
+++ js/main.js
@@ -30,6 +30,36 @@ $(document).ready(function () {
 		}
 	})
 
+   $('.slide__button').on('click', function () {
+     $('.slide__languages').toggleClass('slide__languages--active')
+     $('.slide__button').toggleClass('slide__button--active')
+   })
+
+   $('.slide__lang--en').on('click', function () {
+     $('.slide__button').text('en');
+     $('.slide__language--ru').addClass('slide__language--ru--active')
+     $('.slide__language--en').removeClass('slide__language--en--active')
+     $('.slide__language--aze').addClass('slide__language--aze--active')
+   })
+
+   $('.slide__lang--ru').on('click', function () {
+     $('.slide__button').text('ru');
+     $('.slide__language--en').addClass('slide__language--en--active');
+     $('.slide__language--ru').removeClass('slide__language--ru--active');
+     $('.slide__language--aze').addClass('slide__language--aze--active');
+   })
+
+   $('.slide__lang--aze').on('click', function () {
+     $('.slide__button').text('aze');
+     $('.slide__language--aze').removeClass('slide__language--aze--active');
+     $('.slide__language--en').addClass('slide__language--en--active');
+     $('.slide__language--ru').addClass('slide__language--ru--active');
+   })
+
+   $('.slide__lang').on('click', function () {
+     $('.slide__languages').removeClass('slide__languages--active')
+   })
+
 })
 
 
